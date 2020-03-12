f624bdd151792645b46935acc0f03ee2e74fed78
siert27 <58687089+siert27@users.noreply.github.com>
2020-03-12 09:29:22 +0100

    Add files via upload

---
+++ H2/opdg1H2/opdg1H2.js
@@ -0,0 +1,8 @@
+var mijnauto = {
+    merk: "ford",
+    type: "mondeo",
+    aantalWielen: 4,
+    kleur: "blauw",
+}
+console.log(mijnauto.kleur);
+document.getElementById("grotekop").innerHTML = mijnauto.kleur
---
+++ H2/opdg2h2/opdg2H2.js
@@ -0,0 +1,23 @@
+
+document.getElementById("blokje1").style.backgroundColor = "BLUE";
+document.getElementById("blokje2").style.backgroundColor = "GREEN";
+document.getElementById("blokje3").style.backgroundColor = "YELLOW";
+document.getElementById("blokje4").style.backgroundColor = "RED";
+
+document.getElementById("blokje1").style.height="100px";
+document.getElementById("blokje1").style.width="100px";
+
+document.getElementById("blokje2").style.height="100px";
+document.getElementById("blokje2").style.width="100px";
+
+document.getElementById("blokje3").style.height="100px";
+document.getElementById("blokje3").style.width="100px";
+
+document.getElementById("blokje3").style.height="100px";
+document.getElementById("blokje3").style.width="100px";
+
+document.getElementById("blokje4").style.height="100px";
+document.getElementById("blokje4").style.width="100px";
+
+
+
